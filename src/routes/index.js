e58e3232b853e5bafe2f532a3ecfcba6fd381e0a
apipiltzin <xaanhach@gmail.com>
2021-09-06 12:48:04 -0500

    Initial commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,20 +7,3 @@ router.get('/', function(req, res, next) {
 });
 export default router;
 
-
-
-
-
-
-
-/**
-var express = require('express');
-var router = express.Router();
-*/
-/* GET home page. */
-/** router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
-*/
